afaccaf341a757de12edf9c0508ee2978754e4c5
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-06-01 22:52:15 -0400

    estrutura da tabela pronta

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 let paleta = document.querySelector(".color-palette");
-let table = document.querySelector("#table");
 
-let lines = 5;
-let column = 10;
-
-function createbox(name){
-let box = document.createElement("div");
-box.className = name;
-document.body.insertBefore(box, paleta);
-table.appendChild(box);
-}
+let pixelBoard = document.querySelector("#pixel-board");
 
 
-//for (let index = 0; index = column; index += 1){
 
-createbox('box');
-//}
+
+
 
 
 
